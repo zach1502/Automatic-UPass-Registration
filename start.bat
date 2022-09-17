17bcb3d8c979d8c20f93cfa5213a6fafc935f1a4
zach1502 <zach1502@users.noreply.github.com>
2022-09-17 14:17:46 -0700

    Batchfile for easier way of running the script

--- start.bat
+++ start.bat
@@ -1,2 +1,15 @@
-python main.py
-pause
+@echo off
+cls
+:start
+echo RUNNING AUTO UPASS REGISTRATION
+SET lastRan=0
+SET month=%date:~4,2%
+SET day=%date:~7,2%
+IF %day% == 16 (
+	IF NOT %month% == %lastRan% (
+		python main.py
+		SET lastRan=%date:~4,2%
+	)
+)
+timeout /t 10000 /nobreak
+goto start
